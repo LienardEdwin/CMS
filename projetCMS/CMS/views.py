51d2a4c212ec7cd92ffb3f3cb65da248c72272cc
Edwin LIENARD <edwin.lienard@outlook.fr>
2018-09-05 16:44:13 +0200

    ajout des articles sur index.html

--- projetCMS/CMS/views.py
+++ projetCMS/CMS/views.py
@@ -1,6 +1,10 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.shortcuts import render_to_response
+from .models import Post
 
-from django.shortcuts import render
+def view_posts(request):
 
-# Create your views here.
+    posts = Post.objects.all()
+    return render_to_response('index.html', {
+        'posts': posts,
+
+    })
