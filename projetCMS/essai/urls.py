31fab79ba2dcbc08a15fec1acaf78f32d880086c
Thomas <thomas.malh@gmail.com>
2018-09-05 14:30:32 +0200

    change dir

--- projetCMS/essai/urls.py
+++ projetCMS/essai/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from demineur.views import vue_partie
+from CMS.views import vue_partie
 
 urlpatterns = [
     path('admin/', admin.site.urls),
