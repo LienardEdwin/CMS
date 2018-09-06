004bcee63fcd6543dbb2c8f82953e8a3797e2bc9
Thomas <thomas.malh@gmail.com>
2018-09-06 14:02:05 +0200

    pictures url saved in database

--- projetCMS/CMS/models.py
+++ projetCMS/CMS/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     post_title = models.CharField(max_length=50)
     post_content = models.CharField(max_length=5000)
     text = models.TextField(blank=True)
+    picture_url = models.CharField(max_length=5000, blank=True )
 
     def __str__(self):
         return f' {self.post_title}'
